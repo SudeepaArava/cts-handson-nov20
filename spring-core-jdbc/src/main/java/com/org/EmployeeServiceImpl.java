116e9c3e3f3ec871e2da73b8ceaf4a3d617393cd
SudeepaArava <sudeepaarava@gmail.com>
2020-11-25 19:12:05 +0530

    spring - jdbc

---
+++ spring-core-jdbc/src/main/java/com/org/EmployeeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.org;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private EmployeeDao employeeDao;
+	
+	@Override
+	public void storeEmployee(Employee emp) {
+
+		employeeDao.store(emp);
+	}
+
+	
+}
